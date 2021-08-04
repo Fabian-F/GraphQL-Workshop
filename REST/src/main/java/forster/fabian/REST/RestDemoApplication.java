7aa089c5fbd767b1b42df7a4e617fd42d16dd6b7
Fabian Forster <fabianforster@hotmail.de>
2021-08-04 09:51:40 +0200

    Added REST Demo as Java Project

---
+++ REST/src/main/java/forster/fabian/REST/RestDemoApplication.java
@@ -0,0 +1,13 @@
+package forster.fabian.REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDemoApplication.class, args);
+	}
+
+}
